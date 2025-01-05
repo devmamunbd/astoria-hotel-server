a874a4dac3ab70f2561c6d8059edc771b1aa11af
devmamunbd <itsmemamunbd@gmail.com>
2025-01-05 10:24:24 +0600

    password hashing done

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -29,7 +29,15 @@ export const userSchema = new Schema({
 });
 
 
-
+userSchema.pre('save', async function(next){
+    const user = this;
+    if (!user.isModified('password')) {
+        return next()
+    };
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next()
+})
 
 
 export const User = model('User', userSchema)
